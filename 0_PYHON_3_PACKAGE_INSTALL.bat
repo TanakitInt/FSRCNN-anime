cac2e9f78b0db252af8081c1b4523a220b4a1c6f
TanakitInt <imTanakit@gmail.com>
2021-01-15 20:41:46 +0700

    Requirements update and Code restoration
    - Also restore a code that accidentally deleted.

--- 0_PYHON_3_PACKAGE_INSTALL.bat
+++ 0_PYHON_3_PACKAGE_INSTALL.bat
@@ -2,4 +2,6 @@
 pip install --upgrade pip
 timeout 3
 pip install -r _requirements/requirements.txt
+timeout 3
+conda install --file _requirements/conda.txt
 
--- prepare-data-sharpening.py
+++ prepare-data-sharpening.py
@@ -20,5 +20,24 @@ def sharpening(path):
         cv2.imwrite("dataset/original-sharpened/{}".format(file), denoise)
 
 sharpening("dataset/original")
-sharpening("dataset/test")
+
+
+def sharpening_test(path):	
+
+    for file in os.listdir(path):	
+
+        img = cv2.imread(path + '/' + file)	
+
+        # sharpening	
+        blur_img = cv2.GaussianBlur(img, (0, 0), 5)	
+        sharpened = cv2.addWeighted(img, 1.5, blur_img, -0.5, 0)	
+
+        # denoising	
+        denoise = cv2.fastNlMeansDenoisingColored(sharpened, dst=None, h=10, hColor=10, templateWindowSize=3, searchWindowSize=9)	
+
+        print("Saving {}".format(file))	
+
+        cv2.imwrite("dataset/test-sharpened/{}".format(file), denoise)	
+
+sharpening_test("dataset/test")
 
